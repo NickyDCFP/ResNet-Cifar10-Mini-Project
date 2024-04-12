3f233196f466da5554105496a8b12e2f59f40941
Nicky Kriplani <aniketnickykriplani@gmail.com>
2024-04-12 12:58:08 -0400

    merging with main

--- train.py
+++ train.py
@@ -1,16 +1,14 @@
-import os
 import torch
 from torch import nn
 from torch.nn.parameter import Parameter
 from torch.utils.data import DataLoader
 from torch.optim import Adam, AdamW, SGD, Optimizer
 import pandas as pd
-from collections import OrderedDict
 from argparse import Namespace
 from typing import Iterator
 
 from constants import MAX_PARAMS
-from resnet import ResNet, BasicBlock, ResNet101
+from resnet import ResNet, BasicBlock
 from data import get_dataset
 
 def train(args):
@@ -57,8 +55,8 @@ def train_model(
     opt: Optimizer = get_optim(
         args.optim,
         model.parameters(),
-        args.model_lr,
-        args.model_weight_decay
+        args.lr,
+        args.weight_decay
     )
     n_params = sum(p.numel() for p in model.parameters())
     assert n_params <= MAX_PARAMS, (
@@ -66,23 +64,21 @@ def train_model(
         f"but model has {n_params} parameters."
     )
     model = model.to(device)
+    history: pd.DataFrame = pd.DataFrame(columns=metrics)
 
-    model_loss: nn.CrossEntropyLoss = nn.CrossEntropyLoss()
-    for epoch in range(args.model_epochs):
+    loss: nn.CrossEntropyLoss = nn.CrossEntropyLoss(label_smoothing=0.2)
+    for epoch in range(args.epochs):
         train_one_epoch(
             model,
             train_dataloader,
             val_dataloader,
-            model_loss,
+            loss,
             opt,
             epoch,
             device,
-            model_history,
-            args,
-            teacher
+            history,
         )
-    model_history: pd.DataFrame = pd.DataFrame(columns=metrics)
-    model_history.to_csv(f"model_history_{args.csv_suffix}.csv")
+    history.to_csv(f"history_{args.csv_suffix}.csv")
     
 def get_optim(
     name: str,
@@ -106,27 +102,15 @@ def train_one_epoch(
     epoch: int,
     device: str,
     history: pd.DataFrame,
-    args: Namespace,
-    teacher: ResNet = None,
 ) -> None:
     train_loss: float = 0.0
     val_loss: float = 0.0
-    hard_targets: bool = teacher is None or \
-                         (args.epochs - epoch <= args.model_hard_epochs)
     x: torch.Tensor; y: torch.Tensor
     model.train()
-    if teacher is None:
-        print(f"Teacher epoch {epoch + 1}")
-    else:
-        teacher.eval()
-        print(f"Student epoch {epoch + 1}")
+    print(f"Epoch {epoch + 1}")
     for _, (x, y) in enumerate(train_dataloader):
         x = x.to(device)
-        if hard_targets:
-            y = y.to(device)
-        else:
-            with torch.no_grad():
-                y = teacher(x)
+        y = y.to(device)
         opt.zero_grad()
         y_pred: torch.Tensor = model(x)
         fit: torch.Tensor = loss(y_pred, y)
@@ -138,11 +122,7 @@ def train_one_epoch(
     for _, (x, y) in enumerate(val_dataloader):
         with torch.no_grad():
             x = x.to(device)
-            if hard_targets:
-                y = y.to(device)
-            else:
-                with torch.no_grad():
-                    y = teacher(x)
+            y = y.to(device)
             y_pred = model(x)
             fit = loss(y_pred, y)
             val_loss += fit.item()
